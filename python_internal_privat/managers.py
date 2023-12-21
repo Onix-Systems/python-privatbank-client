fe23be23c9f62ed4d30b4029e2122c183bac8c17
Ihor Sotnyk <ihor.sotnyk@onix-systems.com>
2023-12-21 22:17:04 +0200

    Lint

--- python_internal_privat/managers.py
+++ python_internal_privat/managers.py
@@ -1,7 +1,7 @@
-import requests
 import json
+import requests
+from typing import Dict
 from datetime import datetime
-from typing import Tuple, List, Dict
 
 from .config import (
     PRIVATBANK_CURRENCY_CASHE_RATE_URI,
@@ -11,7 +11,6 @@ from .config import (
     PRIVATBANK_STATEMENT_URI,
     PRIVATBANK_STATEMENT_URI_BODY,
     PRIVATBANK_PAYMENT_URI,
-
     DOCUMENT_NUMBER,
     DOCUMENT_TYPE,
     PAYMENT_CCY,
@@ -24,7 +23,6 @@ from .config import (
 
 
 class PrivatManager:
-
     def __init__(self, token=None, iban=None):
         self._token = token
         self._iban = iban
@@ -48,55 +46,55 @@ class PrivatManager:
     _recipient_nceo = RECIPIENT_NCEO
 
     @property
-    def token(self):
+    def token(self) -> str:
         return self._token
-    
+
     @token.setter
     def token(self, new_token):
         self._token = new_token
 
     @property
-    def iban(self):
+    def iban(self) -> str:
         return self._iban
-    
+
     @iban.setter
     def iban(self, new_iban):
         self._iban = new_iban
 
     @property
-    def privat_currency_cashe_rate_uri(self):
+    def privat_currency_cashe_rate_uri(self) -> str:
         return self._privat_currency_cashe_rate_uri
-    
+
     @privat_currency_cashe_rate_uri.setter
     def privat_currency_cashe_rate_uri(self, new_uri):
         self._privat_currency_cashe_rate_uri = new_uri
-    
+
     @property
-    def privat_currency_non_cashe_rate_uri(self):
+    def privat_currency_non_cashe_rate_uri(self) -> str:
         return self._privat_currency_non_cashe_rate_uri
-    
+
     @privat_currency_non_cashe_rate_uri.setter
     def privat_currency_non_cashe_rate_uri(self, new_uri):
         self._privat_currency_non_cashe_rate_uri = new_uri
 
     @property
-    def privat_balance_uri(self):
+    def privat_balance_uri(self) -> str:
         return self._privat_balance_uri
-    
+
     @privat_balance_uri.setter
     def privat_balance_uri(self, new_uri):
         self._privat_balance_uri = new_uri
 
     @property
-    def privat_statement_uri(self):
+    def privat_statement_uri(self) -> str:
         return self._privat_statement_uri
-    
+
     @privat_statement_uri.setter
     def privat_statement_uri(self, new_uri):
         self._privat_statement_uri = new_uri
 
     @property
-    def privat_payment_uri(self):
+    def privat_payment_uri(self) -> str:
         return self._privat_payment_uri
 
     @privat_payment_uri.setter
@@ -104,7 +102,7 @@ class PrivatManager:
         self._privat_payment_uri = new_uri
 
     @property
-    def privat_balance_uri_body(self):
+    def privat_balance_uri_body(self) -> str:
         return self._privat_balance_uri_body
 
     @privat_balance_uri_body.setter
@@ -112,84 +110,84 @@ class PrivatManager:
         self._privat_balance_uri_body = new_uri_body
 
     @property
-    def privat_statement_uri_body(self):
+    def privat_statement_uri_body(self) -> str:
         return self._privat_statement_uri_body
-    
+
     @privat_statement_uri_body.setter
     def privat_statement_uri_body(self, new_uri_body):
         self._privat_statement_uri_body = new_uri_body
-    
+
     @property
-    def document_number(self):
+    def document_number(self) -> str:
         return self._document_number
-    
+
     @document_number.setter
     def document_number(self, new_document_number):
         self._document_number = new_document_number
 
     @property
-    def document_type(self):
+    def document_type(self) -> str:
         return self._document_type
-    
+
     @document_type.setter
     def document_type(self, new_document_type):
         self._document_type = new_document_type
 
     @property
-    def payment_ccy(self):
+    def payment_ccy(self) -> str:
         return self._payment_ccy
-    
+
     @payment_ccy.setter
     def payment_ccy(self, new_payment_ccy):
         self._payment_ccy = new_payment_ccy
 
     @property
-    def payment_destination(self):
+    def payment_destination(self) -> str:
         return self._payment_destination
-    
+
     @payment_destination.setter
     def payment_destination(self, new_payment_destination):
         self._payment_destination = new_payment_destination
-    
+
     @property
-    def payment_naming(self):
+    def payment_naming(self) -> str:
         return self._payment_naming
-    
+
     @payment_naming.setter
     def payment_naming(self, new_payment_naming):
         self._payment_naming = new_payment_naming
 
     @property
-    def recipient_ify(self):
+    def recipient_ify(self) -> str:
         return self._recipient_ify
-    
+
     @recipient_ify.setter
     def recipient_ify(self, new_recipient_ify):
         self._recipient_ify = new_recipient_ify
 
     @property
-    def recipient_ify_text(self):
+    def recipient_ify_text(self) -> str:
         return self._recipient_ify_text
-    
+
     @recipient_ify_text.setter
     def recipient_ify_text(self, new_recipient_ify_text):
         self._recipient_ify_text = new_recipient_ify_text
 
     @property
-    def recipient_nceo(self):
+    def recipient_nceo(self) -> str:
         return self._recipient_nceo
-    
+
     @recipient_nceo.setter
     def recipient_nceo(self, new_recipient_nceo):
         self._recipient_nceo = new_recipient_nceo
 
     @classmethod
-    def session(cls):
+    def session(cls) -> requests.sessions.Session:
         return requests.Session()
-    
+
     @staticmethod
-    def __date(period: int) -> Tuple[str|Dict]:
-        _day = 86400   # 1 day (UNIX)
+    def __date(period: int) -> Dict:
+        _day = 86400  # 1 day (UNIX)
         try:
             time_delta = int(datetime.now().timestamp()) - (period * _day)
             dt_object = datetime.fromtimestamp(time_delta)
@@ -197,14 +195,13 @@ class PrivatManager:
             month = dt_object.strftime("%m")
             day = dt_object.strftime("%d")
             date = f"{day}-{str(month)}-{year}"
-            return date
+            payload = {"date": date}
+            return payload
         except Exception as exc:
-            exception = {
-                "detail": str(exc)
-            }
+            exception = {"detail": str(exc)}
             return exception
 
-    def get_currency(self, cashe_rate: bool) -> Tuple[Dict|List[Dict]]:
+    def get_currency(self, cashe_rate: bool) -> Dict:
         try:
             session = self.session()
             if cashe_rate:
@@ -214,25 +211,21 @@ class PrivatManager:
             response = session.get(uri)
             code = response.status_code
             response.raise_for_status()
-            return response.json()  
+            payload = {"code": code, "detail": response.json()}
+            return payload
         except requests.exceptions.HTTPError as exc:
-            error_response = {
-                "code": code,
-                "detail": str(exc)
-            }
+            error_response = {"code": code, "detail": str(exc)}
             return error_response
         except Exception as exc:
-            exception = {
-                "detail": str(exc)
-            }
-            return exception                 
+            exception = {"detail": str(exc)}
+            return exception
 
-    def get_client_info(self) -> Tuple[Dict|List[Dict]]:
+    def get_client_info(self) -> Dict:
         try:
             session = self.session()
             token = self.token
             iban = self.iban
-            date = self.__date(0)
+            date = self.__date(0).get("date")
             balance_uri = self.privat_balance_uri
             uri_body = self.privat_balance_uri_body
             uri = uri_body.format(balance_uri, iban, date)
@@ -240,56 +233,48 @@ class PrivatManager:
             response = session.get(uri, headers=headers)
             code = response.status_code
             response.raise_for_status()
-            return response.json()  
+            payload = {"code": code, "detail": response.json()}
+            return payload
         except requests.exceptions.HTTPError as exc:
-            error_response = {
-                "code": code,
-                "detail": str(exc)
-            }
+            error_response = {"code": code, "detail": str(exc)}
             return error_response
         except Exception as exc:
-            exception = {
-                "detail": str(exc)
-            }
+            exception = {"detail": str(exc)}
             return exception
 
-    def get_balance(self) -> Tuple[Dict|List[Dict]]:
+    def get_balance(self) -> Dict:
         try:
-            response = self.get_client_info()
-            balance = {
-                "balance": response["balances"][0]["balanceOutEq"]
-            }
+            client_info = self.get_client_info()
+            code = client_info.get("code")
+            payload = client_info.get("detail")
+            balance = {"code": code, "balance": payload["balances"][0]["balanceOutEq"]}
             return balance
         except Exception:
-            return response
-        
-    def get_statement(self, period: int, limit: int) -> Tuple[Dict|List[Dict]]:
+            return client_info
+
+    def get_statement(self, period: int, limit: int) -> Dict:
         try:
             session = self.session()
             token = self.token
             iban = self.iban
             statement_uri = self.privat_statement_uri
             uri_body = self.privat_statement_uri_body
-            date = self.__date(period)
+            date = self.__date(period).get("date")
             uri = uri_body.format(statement_uri, iban, date, limit)
             headers = {"token": token}
             response = session.get(uri, headers=headers)
             code = response.status_code
             response.raise_for_status()
-            return response.json()  
+            payload = {"code": code, "detail": response.json()}
+            return payload
         except requests.exceptions.HTTPError as exc:
-            error_response = {
-                "code": code,
-                "detail": str(exc)
-            }
+            error_response = {"code": code, "detail": str(exc)}
             return error_response
         except Exception as exc:
-            exception = {
-                "detail": str(exc)
-            }
+            exception = {"detail": str(exc)}
             return exception
-        
-    def create_payment(self, recipient: str, amount: float) -> Tuple[Dict|List[Dict]]:
+
+    def create_payment(self, recipient: str, amount: float) -> Dict:
         try:
             session = self.session()
             token = self.token
@@ -301,20 +286,16 @@ class PrivatManager:
             response = session.post(uri, headers=headers, data=data)
             code = response.status_code
             response.raise_for_status()
-            return response.json() 
+            payload = {"code": code, "detail": response.json()}
+            return payload
         except requests.exceptions.HTTPError as exc:
-            error_response = {
-                "code": code,
-                "detail": str(exc)
-            }
+            error_response = {"code": code, "detail": str(exc)}
             return error_response
         except Exception as exc:
-            exception = {
-                "detail": str(exc)
-            }
+            exception = {"detail": str(exc)}
             return exception
-    
-    def __payment_body(self, recipient: str, amount: float, iban: str) -> Tuple[Dict]:
+
+    def __payment_body(self, recipient: str, amount: float, iban: str) -> Dict:
         try:
             payment_body = {
                 "document_number": self._document_number,
@@ -327,11 +308,9 @@ class PrivatManager:
                 "payment_destination": self._payment_destination,
                 "payer_account": iban,
                 "payment_ccy": self._payment_ccy,
-                "document_type": self._document_type
+                "document_type": self._document_type,
             }
             return payment_body
         except Exception as exc:
-            exception = {
-                "detail": str(exc)
-            }
+            exception = {"detail": str(exc)}
             return exception
